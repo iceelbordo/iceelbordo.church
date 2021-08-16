2a80481a355be2af29f26e8ed6913caacafe662e
Esteban Nieva <estebannieva.dev@gmail.com>
2021-08-16 01:51:55 +0100

    feat: Create Container component
    Refs #9

---
+++ components/Container.vue
@@ -0,0 +1,43 @@
+<template>
+  <c-box
+    :height="height"
+    px="4"
+    :pt="pt"
+    :pb="pb"
+    mx="auto"
+    :max-width="[
+      '100%',  // base
+      '30rem', // sm (30em)
+      '48rem', // md (48em)
+      '62rem', // lg (62em)
+      '80rem'  // xl (80em)
+    ]"
+  >
+    <slot></slot>
+  </c-box>
+</template>
+
+<script>
+import { CBox } from '@chakra-ui/vue'
+
+export default {
+  name: 'Container',
+  components: {
+    CBox
+  },
+  props: {
+    pt: {
+      type: String,
+      default: '0'
+    },
+    pb: {
+      type: String,
+      default: '0'
+    },
+    height: {
+      type: String,
+      default: 'auto'
+    }
+  }
+}
+</script>
