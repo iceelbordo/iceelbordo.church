7638851c601c3ef6c010d09af79cb856b2b8c802
Esteban Nieva <estebannieva.dev@gmail.com>
2021-08-16 01:38:31 +0100

    feat: Create BaseLayout component
    Refs #8

---
+++ components/BaseLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <c-box>
+    <slot name="nav">
+    </slot>
+    <slot></slot>
+    <slot name="footer">
+    </slot>
+  </c-box>
+</template>
+
+<script>
+export default {
+  name: 'BaseLayout'
+}
+</script>
