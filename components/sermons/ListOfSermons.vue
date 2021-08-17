4226a448d42acf1a1a073e4c37dea0f91309afb1
Esteban Nieva <estebannieva.dev@gmail.com>
2021-08-17 02:28:59 +0100

    feat: Create ListOfSermons component
    Refs #15

---
+++ components/sermons/ListOfSermons.vue
@@ -0,0 +1,27 @@
+<template>
+  <c-box>
+    <c-box class="space-y-4">
+      <slot></slot>
+      <slot name="scroll"></slot>
+    </c-box>
+  </c-box>
+</template>
+
+<script>
+import { CBox } from '@chakra-ui/vue'
+
+export default {
+  name: 'ListOfSermons',
+  components: {
+    CBox
+  }
+}
+</script>
+
+<style scoped>
+.space-y-4 > :not([hidden]) ~ :not([hidden]) {
+  --space-y-reverse: 0;
+  margin-top: calc(1rem * calc(1 - var(--space-y-reverse)));
+  margin-bottom: calc(1rem * var(--space-y-reverse));
+}
+</style>
