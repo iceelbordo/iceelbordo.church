3a68107948245c43ae7d04c014b8183dfcd7f084
Esteban Nieva <estebannieva.dev@gmail.com>
2021-08-20 01:44:10 +0100

    feat: Create meet Item component
    Refs #19

---
+++ components/meets/Item.vue
@@ -0,0 +1,95 @@
+<template>
+  <c-stack
+    spacing="4"
+  >
+    <c-stack
+      spacing="1"
+    >
+      <c-text
+        as="samp"
+        fontSize="xs"
+      >
+        {{ `${ day() } ${ month() } ${ year() }` }}
+      </c-text>
+      <c-heading
+        as="h2"
+        size="md"
+        fontSize="md"
+        fontWeight="medium"
+      >
+        {{ item.title }}
+      </c-heading>
+      <c-text
+        fontSize="md"
+      >
+        {{ item.description }}
+      </c-text>
+    </c-stack>
+    <c-box>
+      <c-button
+        variant-color="blue"
+        size="sm"
+        variant="outline"
+        rounded="full"
+        :px="[
+          '0.75rem',  // base
+          '0.875rem', // sm
+          '0.75rem'   // md
+        ]"
+        position="static"
+        @click="onPlayClick"
+      >
+        <c-icon
+          name="play"
+          size="4"
+          color="blue"
+        />
+      </c-button>
+    </c-box>
+  </c-stack>
+</template>
+
+<script>
+import { CBox, CButton, CFlex, CHeading, CIconButton, CStack, CText } from '@chakra-ui/vue'
+import eventBus from '@/use/eventBus'
+import { useDate } from '@/use/hooks'
+
+export default {
+  name: 'Item',
+  components: {
+    CBox,
+    CButton,
+    CFlex,
+    CHeading,
+    CIconButton,
+    CStack,
+    CText
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    const { day, month, year } = useDate({ value: this.item.recordedAt })
+
+    return {
+      day, month, year
+    }
+  },
+  methods: {
+    onPlayClick: function(event) {
+      let regexp = /svg|path/i
+      if (event.target.hasAttribute('data-chakra-component') || regexp.test(event.target.tagName)) {
+
+        const event = new CustomEvent('click', {
+          bubbles: true,
+          detail: this.item.video
+        })
+        eventBus.$emit('play:video', event)
+      }
+    }
+  }
+}
+</script>
