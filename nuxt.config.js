0347f68e5790da172ed8b680390253b977e05aaa
Esteban Nieva <estebannieva.dev@gmail.com>
2021-08-28 02:42:37 +0100

    feat: Rename GraphQL server
    Refs #30

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint: process.env.GRAPHQL_URL || 'http://localhost:1337/graphql',
+        httpEndpoint: process.env.GRAPHQL_URL || 'http://strapi:1337/graphql',
       }
     }
   },
